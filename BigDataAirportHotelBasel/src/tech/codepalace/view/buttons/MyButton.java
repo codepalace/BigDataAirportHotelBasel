90e68c59b152892b81b956f121dedc0a108c48fd
Antonio Estevez Gonzalez <captalohoy@gmail.com>
2022-01-23 21:13:08 +0100

    New Class MyButton extends JButton
    Custom button with picture or picture as button. We can apply opacity,
    and effects when hovering the mouse over the button.

---
+++ BigDataAirportHotelBasel/src/tech/codepalace/view/buttons/MyButton.java
@@ -0,0 +1,22 @@
+package tech.codepalace.view.buttons;
+
+import javax.swing.JButton;
+
+
+/**
+ * 
+ * @author Antonio Estevez Gonzalez
+ * 
+ * @description Custom button, with photo as button, opacity options and effects when hovering the mouse over the button.
+ *
+ */
+
+public class MyButton extends JButton {
+
+	/**
+	 * 
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
