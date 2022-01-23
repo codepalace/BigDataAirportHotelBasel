9143d5399d57c21ba567cac3581b041d38b0e28b
Antonio Estevez Gonzalez <captalohoy@gmail.com>
2022-01-23 21:10:15 +0100

    New Class LogicModelStartFrame for the logic instructions.
    This class will contain most of the main frame
    BigDataAirportHotelBaselStartFrame logic.

---
+++ BigDataAirportHotelBasel/src/tech/codepalace/model/LogicModelStartFrame.java
@@ -0,0 +1,13 @@
+package tech.codepalace.model;
+
+
+/**
+ * 
+ * @author Antonio Estevez Gonzalez
+ * 
+ * @description This class will contain most of the main frame logic
+ *
+ */
+public class LogicModelStartFrame {
+
+}
