4e8c502f26265fb1be2bed768b6d41698bd73a93
Antonio Estevez Gonzalez <captalohoy@gmail.com>
2022-01-23 19:35:23 +0100

    New Class BigDataAirportHotelBaselStartFrame First GUI Class

---
+++ BigDataAirportHotelBasel/src/tech/codepalace/view/frames/BigDataAirportHotelBaselStartFrame.java
@@ -0,0 +1,20 @@
+package tech.codepalace.view.frames;
+
+import javax.swing.JFrame;
+
+/**
+ * 
+ * @author Antonio Estevez Gonzalez
+ * 
+ *@version 1.0.0 Jan, 23, 2022 - 7:34:00 PM
+ *
+ *@description Main GUI Class, extends JFrame. First Frame displayed when we start the application.
+ */
+public class BigDataAirportHotelBaselStartFrame extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
