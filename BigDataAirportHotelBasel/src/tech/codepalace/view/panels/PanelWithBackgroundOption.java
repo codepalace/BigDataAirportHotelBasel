9b2d56c998346b2984e36fc73386a6dd993aa282
Antonio Estevez Gonzalez <captalohoy@gmail.com>
2022-01-23 21:03:23 +0100

    New Class PanelWithBAckgroundOption for JPanel with background

---
+++ BigDataAirportHotelBasel/src/tech/codepalace/view/panels/PanelWithBackgroundOption.java
@@ -0,0 +1,20 @@
+package tech.codepalace.view.panels;
+
+import javax.swing.JPanel;
+
+
+/**
+ * 
+ * @author Antonio Estevez Gonzalez
+ * 
+ * @description Class extends JPanel and it will be used to set a Background to the JPanel. 
+ *
+ */
+public class PanelWithBackgroundOption extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
