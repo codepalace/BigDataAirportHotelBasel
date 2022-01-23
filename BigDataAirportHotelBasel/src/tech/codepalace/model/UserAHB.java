fef8a74e3c5d220c92eb75880e952c440055ffce
Antonio Estevez Gonzalez <captalohoy@gmail.com>
2022-01-23 21:07:18 +0100

    New Class UserAHB to configure user data and to get some properties.
    This class will be use to setup some user data and to get some user data
    or properties.

---
+++ BigDataAirportHotelBasel/src/tech/codepalace/model/UserAHB.java
@@ -0,0 +1,13 @@
+package tech.codepalace.model;
+
+/**
+ * 
+ * @author Antonio Estevez Gonzalez
+ * 
+ * @description UserAHB Class it will be used to configure the user data and to obtain certain properties of the logged user.
+ *
+ */
+
+public class UserAHB {
+
+}
