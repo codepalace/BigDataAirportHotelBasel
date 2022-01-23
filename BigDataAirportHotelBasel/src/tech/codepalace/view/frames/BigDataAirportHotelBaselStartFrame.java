896cc2e5fd2f334930f40c8e08a5e6e269d27cd2
Antonio Estevez Gonzalez <captalohoy@gmail.com>
2022-01-23 21:21:30 +0100

    we create the first variables we need in GUI
    We create some variables we need in the Class
    BigDataAirportHotelBaselStartFrame.
    
    - PanelWithBackgroundOption to put a picture in the Background of a
    JPanel.
    
    - We create some JPanels, for the top, center, south, panel for the Name
    of the Hotel, buttonsPanel etc.
    
    - Some JLabel to apply some pictures on it.
    
    - JButtons to interact with the user.
    
    - JLabel loginUserText to display the logged user.
    
    - JLabel cadillac and faceIcon to display a car in the GUI and a Face.
    
    - Image myImage to set the Image to JLabel.

--- BigDataAirportHotelBasel/src/tech/codepalace/view/frames/BigDataAirportHotelBaselStartFrame.java
+++ BigDataAirportHotelBasel/src/tech/codepalace/view/frames/BigDataAirportHotelBaselStartFrame.java
@@ -1,6 +1,14 @@
 package tech.codepalace.view.frames;
 
+import java.awt.Image;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import tech.codepalace.view.buttons.MyButton;
+import tech.codepalace.view.panels.PanelWithBackgroundOption;
 
 /**
  * 
@@ -16,5 +24,35 @@ public class BigDataAirportHotelBaselStartFrame extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	
+	//we create an instance of a background panel for our main Gui
+		private PanelWithBackgroundOption panelWithBackgroundOption;
+		
+		//We create the panels we need
+		private JPanel toppanel, centerPanel, southPanel, panelNameHotel, buttonsPanelSouthContainer, buttonsPanelSouth, menuButtonPanel,
+			containerPicsHotel;
+		
+		//Some JLabel for the name of the Hotel, icon, etc.
+		private JLabel nameHotelImage, picHoteLabel,  picHotelLabelRight, iconHotel, redLineNorth, redLineSouth;
+		
+		//main buttons to access the different databases of the program
+		public JButton  logoutButton, parkingButton, fundsachenButton, phonebookButton, fitnessButton;
+		
+		
+		//JLabel to display the user logged in
+		public JLabel loginUserText;
+		
+		
+		//Some MyButton to access other application options kontoVerwalten(manage account) benutzerVerwalten(Manage User)
+		//I am creating an Application in German language.
+		public MyButton btn_createDB, btn_kontoVerwalten, btn_benutzerVerwalten, btn_exit;
+		
+		
+		//Some other JLabel for a Car picture and a Face Icon
+		private JLabel cadillac, faceIcon;
+		
+		//Variable to set the Image to JLabel
+		private Image myImage;
 
 }
