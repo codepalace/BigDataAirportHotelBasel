117f1f0a6ef5e53d445d2c335cbb5a6d6ef90431
Antonio Estevez Gonzalez <captalohoy@gmail.com>
2022-01-25 00:26:37 +0100

    New Class Main Class with the Method main. Start the application
    We create the different instances necessary to start our first Frame.
    
    We have add some images for the frame. these graphic changes will be
    modified because I am not convinced at all

---
+++ BigDataAirportHotelBasel/src/tech/codepalace/controller/BigDataAHBRunner.java
@@ -0,0 +1,59 @@
+package tech.codepalace.controller;
+
+import javax.swing.SwingUtilities;
+
+import tech.codepalace.model.LogicModelStartFrame;
+import tech.codepalace.model.UserAHB;
+import tech.codepalace.view.frames.BigDataAirportHotelBaselStartFrame;
+
+/**
+* Runner object for the MVC BigDataAHB application.
+* @author Antonio Estevez Gonzalez
+* @version Apr 18, 2014 4:2:17 PM
+*
+*/
+public class BigDataAHBRunner {
+
+	/**
+	 * Main starter method or entry point for the Java program.
+	 * @param args Unused as this is a GUI BigDataAHB app.
+	 */
+	public static void main(String[] args) {
+
+
+		
+		SwingUtilities.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+
+				//Instantiate the main window(The principal GUI JFrame class)
+				BigDataAirportHotelBaselStartFrame bigDataAirportHotelBaselStartFrame = new BigDataAirportHotelBaselStartFrame();
+				
+				//Instantiate UserAHB
+				UserAHB userAHB = new UserAHB();
+				
+				
+				//Instantiate LogicModelStartFrame
+			    LogicModelStartFrame logicModelStartFrame = new LogicModelStartFrame();
+			    
+
+
+						
+				//We instantiate the controller class and pass the required arguments to it.
+				@SuppressWarnings("unused")
+				BigDataAHBStartFrameController bigDataAHBStartFrameController = new BigDataAHBStartFrameController(bigDataAirportHotelBaselStartFrame, userAHB, logicModelStartFrame);
+				
+				
+				
+				bigDataAirportHotelBaselStartFrame.setLocationRelativeTo(null);
+				bigDataAirportHotelBaselStartFrame.setVisible(true);
+			}
+		});
+
+		
+		
+	}
+
+}
+	
