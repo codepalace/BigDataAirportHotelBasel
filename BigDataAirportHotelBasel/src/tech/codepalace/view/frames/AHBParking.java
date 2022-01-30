974a5bfa737a24f289be35a1ef12c9ab2bb06a6d
codepalace <captalohoy@gmail.com>
2022-01-30 23:15:04 +0100

    Some new Variables were created AHBParking Class.
    we initialize the buttons to be able to access them from the controller
    class.
    
    We created the variables for the custom dialog box, jbuttons, jlables,
    jpanels, Object array for the jbuttons options inside the custom dialog
    box. etc.

--- BigDataAirportHotelBasel/src/tech/codepalace/view/frames/AHBParking.java
+++ BigDataAirportHotelBasel/src/tech/codepalace/view/frames/AHBParking.java
@@ -1,5 +1,7 @@
 package tech.codepalace.view.frames;
 
+import java.awt.Dimension;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
@@ -9,7 +11,14 @@ import java.awt.event.KeyListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import tech.codepalace.view.panels.PanelWithBackgroundOption;
 
 
 /**
@@ -28,10 +37,53 @@ public class AHBParking extends JFrame implements ActionListener, KeyListener, W
 	private static final long serialVersionUID = 1L;
 	
 	
+	private PanelWithBackgroundOption panelWithBackgroundOption;
+	
+	
+	//Variable to get the Display Size
+	private Dimension screenSize;
+	
+	private JPanel topPanel, centerPanel, southPanel;
+	
+	private JLabel cadillac;
+	
+	private Image myImage;
+	
+	private ImageIcon exitParkingImageIcon;
+	
+	//Dialog Box To use when the user presses on the cross close the application.
+	public JDialog exitParkingDialog, noExitParkingDialog;
+	
+	//Option Buttons
+	public JButton exitParking, noExitParking;
+	
+	//Array to include the options Jbuttons for the custom Dialog Box
+	private Object [] options;
+	
+	//JPanels for the Custom Dialog Box and for the option buttons.
+	private JPanel panelContainerMessage, panelButtons; 
+	
+	//Panel for the Message
+	private JPanel panelTextCenter;
+	
+	//JLabel for the alert message.
+	private JLabel message; 
+	
+	
+	
 	
 	
 	public AHBParking() {
 		this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		
+		this.panelWithBackgroundOption = new PanelWithBackgroundOption();
+		
+		this.exitParking = new JButton("Ja");
+		this.noExitParking = new JButton("Nein");
+		
+		
+		
+		
 	}
 
 
