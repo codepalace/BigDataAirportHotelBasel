56d2273f23155ad270b4bc64a47ade02e000ffb4
codepalace <estevez75015@gmail.com>
2022-07-20 23:05:16 +0200

    New Objects for Search Results from the Entries in DataBase.
    New image for the Searchbar.
    
    New Class RoundJTextField for rounded JTextField
    
    TopPanelFundSachen: New Object for Searching results in dataBase.

--- BigDataAirportHotelBasel/src/tech/codepalace/view/panels/TopPanelFundSachen.java
+++ BigDataAirportHotelBasel/src/tech/codepalace/view/panels/TopPanelFundSachen.java
@@ -1,7 +1,10 @@
 package tech.codepalace.view.panels;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
 
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
@@ -10,10 +13,12 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.Border;
 
+import tech.codepalace.utility.RoundJTextField;
+
 @SuppressWarnings("serial")
 public class TopPanelFundSachen extends JPanel {
 	
-	private JPanel  centerPanelButtons, containerPanelButton;
+	private JPanel  centerPanelButtons, containerPanelButton, containerSearchSelect;
 	
 	//Border for the centerPanelButtons
 	private Border etchedBorder;
@@ -22,6 +27,12 @@ public class TopPanelFundSachen extends JPanel {
 	
 	private JButton btnHome,  btnParking,  btnFitness,
 	 btnPhonebook,  btnLogout, btnNewFundsachen;
+	
+    //JTextField with rounded Border for the Search Entries
+	private RoundJTextField searchText;
+	
+	//Object container Panel with background picture to add the searchText object.
+	private PanelWithBackgroundOption containerSearch;
 	
 	public TopPanelFundSachen(JButton btnHome, JButton btnParking, JButton btnFitness,
 			JButton btnPhonebook, JButton btnLogout, JButton btnNewFundsachen) {
@@ -51,7 +62,7 @@ public class TopPanelFundSachen extends JPanel {
 		this.containerPanelButton = new JPanel(new BorderLayout());
 		this.containerPanelButton.setOpaque(false);
 		this.containerPanelButton.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-		this.containerPanelButton.add(centerPanelButtons, BorderLayout.SOUTH);
+		this.containerPanelButton.add(centerPanelButtons, BorderLayout.NORTH);
 
 		this.centerPanelButtons.add(this.btnHome);
 		this.centerPanelButtons.add(this.btnParking);
@@ -59,12 +70,44 @@ public class TopPanelFundSachen extends JPanel {
 		this.centerPanelButtons.add(this.btnPhonebook);
 		this.centerPanelButtons.add(this.btnLogout);
 		this.centerPanelButtons.add(this.btnNewFundsachen);
+		
 
 		this.fundsachenImage = new JLabel(new ImageIcon(getClass().getResource("/img/treasure_350x200.png")));
 		
 		this.setLayout(new BorderLayout());
 		
 		this.setOpaque(false);
+		
+		//Initialize the searchText object 14 Column
+		this.searchText = new RoundJTextField(14);
+		this.searchText.setFont(new Font("Verdana", Font.BOLD, 18));
+		this.searchText.setBackground(Color.GRAY);
+		
+		//Initialize the containerSearch Object
+		this.containerSearch = new PanelWithBackgroundOption();
+		
+		//set the image for the background to this container.
+		this.containerSearch.setImage("/img/lupe_black_gray.png");
+		
+		//Setting the size to the containerSearch
+		this.containerSearch.setPreferredSize(new Dimension(340, 50));
+		
+		//Container containerSearchSelect initialize with the FlowLayout align Left.
+		this.containerSearchSelect = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		this.containerSearchSelect.setOpaque(false);
+		
+		//Set Empty Border to push the searchText in the position what we want.
+		this.containerSearch.setBorder(BorderFactory.createEmptyBorder(4, 32, 0, 0));
+		this.containerSearch.setOpaque(false);
+
+		//Add the searchText to the containerSearch
+		this.containerSearch.add(searchText);
+		
+		//And add the containerSearch to the containerSearchSelect
+		this.containerSearchSelect.add(containerSearch);
+		
+		//And add the containerSearchSelect to the containerPanelButton position Center.
+		this.containerPanelButton.add(containerSearchSelect, BorderLayout.CENTER);
 		
 		this.add(this.fundsachenImage, BorderLayout.WEST);
 		this.add(containerPanelButton, BorderLayout.CENTER);
