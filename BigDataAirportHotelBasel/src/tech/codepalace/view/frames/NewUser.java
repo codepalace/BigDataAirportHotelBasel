0d0d0995aa1489040cd0ff61b379fd2033f5ba22
codepalace <estevez75015@gmail.com>
2022-09-09 19:12:15 +0200

    New Design for the NewUser GUI and the two Buttons.
    NewUser: Import the classes for the Design, GridBagLayout. New Method
    addElementsToGUI.
    
    New images for the JButtons(MyButton).

--- BigDataAirportHotelBasel/src/tech/codepalace/view/frames/NewUser.java
+++ BigDataAirportHotelBasel/src/tech/codepalace/view/frames/NewUser.java
@@ -1,11 +1,22 @@
 package tech.codepalace.view.frames;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 
 import javax.swing.ImageIcon;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
+import tech.codepalace.view.buttons.MyButton;
 import tech.codepalace.view.panels.PanelWithBackgroundOption;
 
 /**
@@ -18,6 +29,22 @@ import tech.codepalace.view.panels.PanelWithBackgroundOption;
 public class NewUser extends JDialog {
 	
 	private PanelWithBackgroundOption panelWithBackgroundOption;
+	
+	private JPanel mainJPanel, entriesPanel, containerButtonPanel;
+	
+	private JLabel userNameJLabel, passwordJLabel; 
+	
+	//Image for the modal Window
+		private JLabel imgModal;
+		
+		public MyButton btnSave, btnCancelSave;
+		
+		public JTextField newUserJTextField;
+		public JPasswordField passwordField;
+
+		private GridBagLayout mainGBL, gbl;
+		private GridBagConstraints mainGBC, gbc;
+	
 	
 	public NewUser(UserManager userManager, boolean modal) {
 		
@@ -51,7 +78,61 @@ public class NewUser extends JDialog {
 		 setContentPane(panelWithBackgroundOption);
 		  
 		  setIconImage(new ImageIcon(getClass().getResource("/img/iconoHotel.png")).getImage());
-		
+		  
+		  this.mainJPanel = new JPanel();
+			
+			this.imgModal = new JLabel();
+			this.imgModal.setOpaque(false);
+		  
+			this.imgModal.setIcon(new ImageIcon(getClass().getResource("/img/users.png")));
+			
+			
+			// GridBagLayout for the mainJPanel
+			this.mainGBL = new GridBagLayout();
+
+			// GridBagConstraints for the mainJPanel
+			this.mainGBC = new GridBagConstraints();
+			
+			
+			this.gbl = new GridBagLayout();
+			this.gbc = new GridBagConstraints();
+			
+			// setLayout to the mainJPanel the GridBagLayout
+			this.mainJPanel.setLayout(mainGBL);
+			this.mainJPanel.setOpaque(false);
+			
+			this.entriesPanel =  new JPanel(gbl);
+			this.entriesPanel.setOpaque(false);
+			
+			this.userNameJLabel = new JLabel("Benutzer:");
+			this.userNameJLabel.setPreferredSize(new Dimension(110,20));
+			this.userNameJLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+			this.userNameJLabel.setFont(new Font("Verdana", Font.BOLD, 16));
+			
+			this.passwordJLabel = new JLabel("Password:");
+			this.passwordJLabel.setPreferredSize(new Dimension(110,20));
+			this.passwordJLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+			this.passwordJLabel.setFont(new Font("Verdana", Font.BOLD, 16));
+			
+			
+			this.newUserJTextField = new JTextField(20);
+			this.newUserJTextField.setText("");
+			
+			this.passwordField = new JPasswordField(20);
+			this.passwordField.setText("");
+			
+			
+			//MyButton and containerButtonPanel
+			this.btnSave = new MyButton("/img/btn_speichern_black.png");
+			this.btnCancelSave = new MyButton("/img/abbrechen_login_gui.png");
+			
+			this.containerButtonPanel = new JPanel(new BorderLayout());
+			this.containerButtonPanel.setOpaque(false);
+			
+			
+			
+			//Add all elements to the GUI
+			addElementsToGUI();
 		
 	}
 	
@@ -76,6 +157,75 @@ public class NewUser extends JDialog {
 						
 				
 		}
+		
+		
+		private void addElementsToGUI() {
+			
+			//Setting the elements GridBagConstraints and add to entriesPanel with GridBagLayout
+			
+			this.gbc.insets = new Insets(10, 0, 10, 0);
+			this.gbc.gridx = 0;
+			this.gbc.gridy = 0;
+			this.entriesPanel.add(this.userNameJLabel, gbc);
+			
+
+			this.gbc.gridx = 1;
+			this.gbc.gridy = 0;
+			this.gbc.insets = new Insets(0, 5, 0, 0);
+			this.entriesPanel.add(this.newUserJTextField, gbc);
+			
+			
+			this.gbc.gridx = 0; 
+			this.gbc.gridy = 1; 
+			this.gbc.insets = new Insets(5, 0, 0, 0);
+			this.entriesPanel.add(this.passwordJLabel, gbc);
+			
+
+			this.gbc.gridx = 1;
+			this.gbc.gridy = 1; 
+			this.gbc.insets = new Insets(5, 5, 0, 0);
+			this.entriesPanel.add(this.passwordField, gbc);
+			
+
+		
+			
+			//We add the entriesPanel to the mainJPanel
+			this.mainJPanel.add(this.entriesPanel);
+			
+			
+			//Add the Buttons to the containerButtonPanel
+			this.containerButtonPanel.add(this.btnSave, BorderLayout.EAST);
+			this.containerButtonPanel.add(this.btnCancelSave, BorderLayout.WEST);
+			
+			
+			
+			//Setting the elements GridBagConstraints and add to mainJPanel with GridBagLayout
+			this.mainGBC.insets = new Insets(30, 0, 0, 0);
+			mainGBC.gridx = 0;
+			mainGBC.gridy = 0;
+			mainGBL.setConstraints(imgModal, mainGBC);
+			this.mainJPanel.add(imgModal);
+			
+			mainGBC.gridx = 1;
+			mainGBC.gridy = 0;
+			mainGBL.setConstraints(this.entriesPanel, mainGBC);
+			
+			
+			
+			mainGBC.gridx = 0;
+			mainGBC.gridy = 1;
+			mainGBC.fill = GridBagConstraints.HORIZONTAL;
+			mainGBC.gridwidth = 2;
+			mainGBL.setConstraints(this.containerButtonPanel, mainGBC);
+			
+			//We add the JButton s to the mainJPanel
+			this.mainJPanel.add(containerButtonPanel);
+			
+			this.panelWithBackgroundOption.add(this.mainJPanel, BorderLayout.NORTH);
+			
+			
+		}
+		
 		
 
 }
