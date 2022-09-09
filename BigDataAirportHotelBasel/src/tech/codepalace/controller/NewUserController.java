25d2ab74cf86dfa51324b9d3c29c498e7f29fcfc
codepalace <estevez75015@gmail.com>
2022-09-09 21:54:40 +0200

    New instructions to save new user.
    LogicModelNewUser:
    
    - New Method saveNewUser
    
    NewUserController:
    
    - Implements FocusListener
    - Add Action and Key Listener to the JButtons.
    - actionPerformed Method call to confirmClose and to saveNewUser
    Methods.
    - Setting Opacity to the JButtons.

--- BigDataAirportHotelBasel/src/tech/codepalace/controller/NewUserController.java
+++ BigDataAirportHotelBasel/src/tech/codepalace/controller/NewUserController.java
@@ -2,6 +2,8 @@ package tech.codepalace.controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.WindowEvent;
@@ -15,7 +17,7 @@ import tech.codepalace.view.frames.NewUser;
  * @author Antonio Estevez Gonzalez
  *
  */
-public class NewUserController implements ActionListener, KeyListener, WindowListener {
+public class NewUserController implements ActionListener, KeyListener, WindowListener, FocusListener {
 	
 	private NewUser newUser;
 	private LogicModelNewUser logicModelNewUser;
@@ -27,17 +29,56 @@ public class NewUserController implements ActionListener, KeyListener, WindowLis
 		
 		this.newUser.addWindowListener(this);
 		
+		//Add the Listener for the btnCancelSave button.
+		this.newUser.btnCancelSave.addActionListener(this);
+		this.newUser.btnCancelSave.addKeyListener(this);
+		this.newUser.btnCancelSave.addFocusListener(this);
+		
+		
+		//Add the Listener for the btnSave button.
+		this.newUser.btnSave.addActionListener(this);
+		this.newUser.btnSave.addKeyListener(this);
+		this.newUser.btnSave.addFocusListener(this);
+		
+		
 		
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent e) {}
+	public void actionPerformed(ActionEvent e) {
+		
+		if(e.getSource()==this.newUser.btnCancelSave) {
+			
+			//Close the NewUser GUI.
+			this.newUser.confirmClose();
+		}
+		
+		else if(e.getSource()==this.newUser.btnSave) {
+			
+			//We call the method to save or write the new user information.
+			this.logicModelNewUser.saveNewUser(this.newUser.newUserJTextField.getText(), 
+					new String(this.newUser.passwordField.getPassword()));
+		}
+	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {}
 
 	@Override
-	public void keyPressed(KeyEvent e) {}
+	public void keyPressed(KeyEvent e) {
+		
+		if(e.getSource()==this.newUser.btnCancelSave && e.getKeyCode()==10) {
+			
+			//Close the NewUser GUI.
+			this.newUser.confirmClose();
+		}
+		
+		//We call the method to save or write the new user information.
+		else if(e.getSource()==this.newUser.btnSave && e.getKeyCode() ==10) {
+			this.logicModelNewUser.saveNewUser(this.newUser.newUserJTextField.getText(), 
+					new String(this.newUser.passwordField.getPassword()));
+		}
+	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {}
@@ -69,5 +110,40 @@ public class NewUserController implements ActionListener, KeyListener, WindowLis
 	@Override
 	public void windowDeactivated(WindowEvent e) {
 }
+
+	
+	
+	@Override
+	public void focusGained(FocusEvent e) {
+		
+		if(e.getSource()==this.newUser.btnCancelSave) {
+			
+			//Set the Opacity to 1
+			this.newUser.btnCancelSave.setOpacity(1);
+		}
+		
+		else if(e.getSource()==this.newUser.btnSave) {
+			
+			//Set the Opacity to 1
+			this.newUser.btnSave.setOpacity(1);
+		}
+	}
+
+	@Override
+	public void focusLost(FocusEvent e) {
+		
+		if(e.getSource()==this.newUser.btnCancelSave) {
+			
+			//Set the Opacity to 0.5 back
+			this.newUser.btnCancelSave.setOpacity(0.5f);
+		}
+		
+		else if(e.getSource()==this.newUser.btnSave) {
+			
+			//Set the Opacity to 0.5 back
+			this.newUser.btnSave.setOpacity(0.5f);
+		}
+		
+	}
 
 }
--- BigDataAirportHotelBasel/src/tech/codepalace/model/LogicModelNewUser.java
+++ BigDataAirportHotelBasel/src/tech/codepalace/model/LogicModelNewUser.java
@@ -1,5 +1,7 @@
 package tech.codepalace.model;
 
+import javax.swing.JOptionPane;
+
 /**
  * @description Logic(Model) Class for the NewUser Class
  * @author Antonio Estevez Gonzalez
@@ -9,11 +11,29 @@ public class LogicModelNewUser {
 	
 	private LogicModelUserManager logicModelUserManager;
 	
+	//Variables to store temporarily the information entered by the user(Admin).
+	private String newUserString="", passwordNewUserString="";
+	
 	public LogicModelNewUser(LogicModelUserManager logicModelUserManager) {
 		
 		this.logicModelUserManager = logicModelUserManager;
 		
 		
+		
+	}
+	
+	
+	/**
+	 * @description Method for calling to write new User in the configuration file throw PropertiesWriter
+	 * @param newUserString
+	 * @param passwordNewUserString
+	 */
+	public void saveNewUser(String newUserString, String passwordNewUserString) {
+		this.newUserString = newUserString;
+		this.passwordNewUserString = passwordNewUserString;
+		
+		//For the moment it will be only displayed the information entered by the user.  Instructions for this method are not finished jet.
+		JOptionPane.showMessageDialog(null, "Ready to save New User: " + this.newUserString + " Password New User: " + this.passwordNewUserString);
 		
 	}
 
