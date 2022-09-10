bae18019607aa49229f97f2c5a81e494be9e07c0
Antonio Estevez Gonzalez <captalohoy@gmail.com>
2022-09-10 17:14:23 +0200

    New Method validatePasswordNewUserEntry to validate Password.
    Extra news:
    
    - by the Method saveNewuser will be validated also the call to the
    method validatePasswordNewUserEntry to check if the value entered by the
    user is correct or not.
    
    - We add some new comments to the Methods.
    
    -

--- BigDataAirportHotelBasel/src/tech/codepalace/model/LogicModelNewUser.java
+++ BigDataAirportHotelBasel/src/tech/codepalace/model/LogicModelNewUser.java
@@ -49,7 +49,7 @@ public class LogicModelNewUser {
 		this.passwordNewUserString = passwordNewUserString;
 		
 		//We evaluate if the User Entry is correct in the next commit we validate also the password i go for the ordinary Job.
-		if(validateNewUserEntry(this.newUserString)) {
+		if(validateNewUserEntry(this.newUserString) && validatePasswordNewUserEntry(this.passwordNewUserString)) {
 			JOptionPane.showMessageDialog(null, "User is correct");
 		}
 		
@@ -63,40 +63,55 @@ public class LogicModelNewUser {
 	 */
 	private boolean validateNewUserEntry(String newUserString) {
 		
+		//We store the value in one array so we can check for empty space.
 		this.userNameArray = this.newUserString.toCharArray();
 		
+		//If the user do not enter any username.
 		if(this.newUserString.equalsIgnoreCase("")) {
 			
+			
+			//Invoke a new Thread with the error message.
 			SwingUtilities.invokeLater( () ->  JOptionPane.showMessageDialog(null, "Bitte geben Sie einen Benutzernamen ein"
 					   , "Kritischer Fehler Benutzername", JOptionPane.ERROR_MESSAGE, this.errorImg));
 			
 			
+			//We set the Focus back to the newUserJTextField.
 			this.newUser.newUserJTextField.requestFocus();
 			
+			//Set the value as false.
 			this.newUserEntryCorrect = false;
 			
-		}else {
+		}else {  //Otherwise
 			
+			//For the moment newUserEntryCorrect value = true.
 			this.newUserEntryCorrect = true;
 			
 			//we check that no blank spaces are entered
 			for(int i=0; i< this.userNameArray.length; i++) {
 				
+				//currentIndex has the value from i actual position iterated by the Array.
 				char currentIndex = this.userNameArray[i];
 
+				//If we find a blank space
 				if(currentIndex == ' ') {
 
+					//Value is false
 					this.newUserEntryCorrect = false;
 
 				}
 				
+				//Arrive to the end the Array
 				if (i == userNameArray.length-1) {
 					
+					//If newUserEntryCorrect is not correct
 					if (!this.newUserEntryCorrect) {
 						
+						
+						//We invoke a new Thread with the error message.
 						SwingUtilities.invokeLater( () ->  JOptionPane.showMessageDialog(null, "Leerzeichen sind für den Benutzernamen nicht zulässig\n\nVersuchen Sie es erneut mit einem gültigen Benutzernamen "
 								   , "Kritischer Fehler Benutzername", JOptionPane.ERROR_MESSAGE, this.errorImg));
 						
+						//We give back the focus.
 						this.newUser.newUserJTextField.requestFocus();
 					}
 						
@@ -105,7 +120,73 @@ public class LogicModelNewUser {
 					}
 		}
 		
+		//returning the value.
 		return newUserEntryCorrect;
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	/**
+	 * @description 
+	 * @param passwordNewUserString
+	 * @return
+	 */
+	private boolean validatePasswordNewUserEntry(String passwordNewUserString) {
+		
+		//By this method is the same process as the Method validateNewUserEntry only with the different is for checking the password entered by the user.
+		
+		this.passwordArray = this.passwordNewUserString.toCharArray();
+		
+		
+	if(this.passwordNewUserString.equalsIgnoreCase("")) {
+			
+			SwingUtilities.invokeLater( () ->  JOptionPane.showMessageDialog(null, "Bite geben Sie ein Passwort für den Administrator ein. das Passwortfeld darf nicht leer sein"
+					   , "Kritischer Fehler Passwort", JOptionPane.ERROR_MESSAGE, this.errorImg));
+			
+			
+			this.newUser.passwordField.requestFocus();
+			
+			this.passwordNewUserCorrect = false;
+			
+		}else {
+			
+			this.passwordNewUserCorrect = true;
+			
+			//we check that no blank spaces are entered
+			for(int i=0; i< this.passwordArray.length; i++) {
+				
+				char currentIndex = this.passwordArray[i];
+
+				if(currentIndex == ' ') {
+
+					this.passwordNewUserCorrect = false;
+
+				}
+				
+				if (i == passwordArray.length-1) {
+					
+					if (!this.passwordNewUserCorrect) {
+						
+						SwingUtilities.invokeLater( () ->  JOptionPane.showMessageDialog(null, "Leerzeichen sind für das Passwort nicht zulässig\\n\\nVersuchen Sie es erneut mit einem gültigen Passwort "
+								   , "Kritischer Fehler Passwort", JOptionPane.ERROR_MESSAGE, this.errorImg));
+						
+						this.newUser.passwordField.requestFocus();
+					}
+						
+					}
+					
+					}
+		}
+		
+		
+		
+		return passwordNewUserCorrect;
+		
 		
 	}
 
