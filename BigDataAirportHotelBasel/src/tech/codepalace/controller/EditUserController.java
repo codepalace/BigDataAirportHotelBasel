5507e9b808aaea8cabeee570b486db2236d1f72b
Antonio Estevez Gonzalez <estevez75015@gmail.com>
2022-09-13 22:28:29 +0200

    EditUserController Add Listeners and Override Methods
    EditUserController:
    
    - Implements ActionListener, KeyListener, WindowListener, FocusListener.
    
    - Add Listener to the JButtons belonging to the EditUserGUI Class.
    
    - New instructions by actionPerformed, keyPressed, windowClosing,
    focusGained, focusLost Methods.

--- BigDataAirportHotelBasel/src/tech/codepalace/controller/EditUserController.java
+++ BigDataAirportHotelBasel/src/tech/codepalace/controller/EditUserController.java
@@ -1,5 +1,14 @@
 package tech.codepalace.controller;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
 import tech.codepalace.model.LogicModelEditUser;
 import tech.codepalace.view.frames.EditUserGUI;
 
@@ -8,7 +17,7 @@ import tech.codepalace.view.frames.EditUserGUI;
  * @author Antonio Estevez Gonzalez
  *
  */
-public class EditUserController {
+public class EditUserController implements ActionListener, KeyListener, WindowListener, FocusListener {
 	
 	private EditUserGUI editUserGUI;
 	private LogicModelEditUser logicModelEditUser;
@@ -17,6 +26,147 @@ public class EditUserController {
 		
 		this.editUserGUI = editUserGUI;
 		this.logicModelEditUser = logicModelEditUser;
+		
+		//Add the Listener to the JButtons
+		this.editUserGUI.btnCancelSave.addActionListener(this);
+		this.editUserGUI.btnCancelSave.addKeyListener(this);
+		this.editUserGUI.btnCancelSave.addFocusListener(this);
+		
+		this.editUserGUI.btnSave.addActionListener(this);
+		this.editUserGUI.btnSave.addKeyListener(this);
+		this.editUserGUI.btnSave.addFocusListener(this);
+		
+		this.editUserGUI.changePassword.addActionListener(this);
+		this.editUserGUI.changePassword.addKeyListener(this);
+		this.editUserGUI.changePassword.addFocusListener(this);
+		
+		this.editUserGUI.addWindowListener(this);
+		
+		
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		if(e.getSource()==this.editUserGUI.btnCancelSave) {
+			
+			//Close the GUI
+			this.editUserGUI.confirmClose();
+		}
+		
+		else if(e.getSource()==this.editUserGUI.btnSave) {
+			
+			
+		}
+		
+		else if(e.getSource()==this.editUserGUI.changePassword) {
+			
+			
+		}
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		
+		if(e.getSource()==this.editUserGUI.btnCancelSave && e.getKeyCode()==10) {
+			
+			//Close the GUI
+			this.editUserGUI.confirmClose();
+		}
+		
+		else if(e.getSource()==this.editUserGUI.btnSave && e.getKeyCode()==10) {
+			
+			
+		}
+		
+		else if(e.getSource()==this.editUserGUI.changePassword && e.getKeyCode()==10) {
+			
+			
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {}
+
+	
+	
+	@Override
+	public void windowOpened(WindowEvent e) {}
+
+	@Override
+	public void windowClosing(WindowEvent e) {
+		//Close the GUI
+		this.editUserGUI.confirmClose();
+		
+	}
+
+	@Override
+	public void windowClosed(WindowEvent e) {}
+
+	@Override
+	public void windowIconified(WindowEvent e) {}
+
+	@Override
+	public void windowDeiconified(WindowEvent e) {}
+
+	@Override
+	public void windowActivated(WindowEvent e) {}
+
+	@Override
+	public void windowDeactivated(WindowEvent e) {}
+
+	
+	
+	
+	@Override
+	public void focusGained(FocusEvent e) {
+		
+		//Change the opacity to the JButtons to 1
+		if(e.getSource()==this.editUserGUI.btnCancelSave) {
+			
+			this.editUserGUI.btnCancelSave.setOpacity(1);
+		}
+		
+		else if(e.getSource()==this.editUserGUI.btnSave) {
+			
+			this.editUserGUI.btnSave.setOpacity(1);
+		}
+		
+		else if(e.getSource()==this.editUserGUI.changePassword) {
+			
+			this.editUserGUI.changePassword.setOpacity(1);
+		}
+		
+		
+		
+	}
+
+	
+	
+	
+	@Override
+	public void focusLost(FocusEvent e) {
+		
+		//Change the opacity to the JButtons to 0.5
+		if(e.getSource()==this.editUserGUI.btnCancelSave) {
+			
+			this.editUserGUI.btnCancelSave.setOpacity(0.5f);
+		}
+		
+		else if(e.getSource()==this.editUserGUI.btnSave) {
+			
+			this.editUserGUI.btnSave.setOpacity(0.5f);
+		}
+		
+		else if(e.getSource()==this.editUserGUI.changePassword) {
+			
+			this.editUserGUI.changePassword.setOpacity(0.5f);
+		}
 	}
 
 }
