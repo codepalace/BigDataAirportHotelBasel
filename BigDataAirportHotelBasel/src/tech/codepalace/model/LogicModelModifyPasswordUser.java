5bacc948964c23cd2c7bf2f2377bb89a50dc7244
Antonio Estevez Gonzalez <estevez75015@gmail.com>
2022-09-23 11:20:22 +0200

    LogicModelModifyPassworUser new instructions to modify by one Method.
    new instructions by modifyPasswordUser Method. We follow the same
    methodology like the second same method with different signature.

--- BigDataAirportHotelBasel/src/tech/codepalace/model/LogicModelModifyPasswordUser.java
+++ BigDataAirportHotelBasel/src/tech/codepalace/model/LogicModelModifyPasswordUser.java
@@ -24,12 +24,6 @@ public class LogicModelModifyPasswordUser {
 	//boolean for the empty spaces newPassword. 
 	private boolean blankSpaceNewPassword = false;
 	
-	//Variable to evaluate if the password match
-	private boolean passwordMatch = false;
-	
-	//Variable to evaluate if the password is empty
-	private boolean emptyPassword = false;
-	
 	//boolean for the new password correct.
 	private boolean newPasswordCorrect = false;
 
@@ -116,126 +110,27 @@ public class LogicModelModifyPasswordUser {
 		//Set the value for the newPasswordCharArray
 		this.newPasswordCharArray = this.newPassword.toCharArray();
 		
-		this.passwordMatch = false;
-		
+		//Initialize the value
 		this.blankSpaceNewPassword = false;
 		
+		//Initialize the value
 		this.newPasswordCorrect = false;
 		
-		
-		
-		//Evaluate if the newPassword and newPasswordConfirmation are not identical.
-		if(!this.newPassword.equalsIgnoreCase(this.newPasswordConfirmation)) {
-			
-			//Invoke a new Thread with the error message.
-			SwingUtilities.invokeLater( () ->  JOptionPane.showMessageDialog(null, "die eingegebenen passwörter stimmen nicht überein. bitte versuchen Sie es nochmal"
-					   , "Passwörter stimmen nicht überein", JOptionPane.ERROR_MESSAGE, this.errorImg));
-			
-			this.passwordMatch = false;
-			this.newPasswordCorrect = false;
-			
-		}else {
-			this.passwordMatch = true;
-			this.newPasswordCorrect = true;
-			
-		}
-		
-		
-		
-		//Evaluate if the new password is not empty
-		if (this.newPassword.equalsIgnoreCase("")) {
+		//Check if something is not OK
+		if(checkNewPasswordEmpty() || checkNewPasswordConfirmationEmpty()
+				   || checkNewPasswordAndPasswordConfirmationMatch()
+				   || checkBlankSpaceNewPassword()) {
 
-			// Invoke a new Thread with the error message.
-			SwingUtilities.invokeLater(() -> JOptionPane.showMessageDialog(null,
-					"Das Passwort darf nicht leer sein. bitte versuchen Sie es nochmal", "Kein Passwort Eingegeben",
-					JOptionPane.ERROR_MESSAGE, this.errorImg));
-			
-			this.emptyPassword = true;
-			this.newPasswordCorrect = false;
-		
+				// Invoke a new Thread with the error message.
+				SwingUtilities.invokeLater(() -> JOptionPane.showMessageDialog(null,
+				this.errorMessage,
+				"Passwort kann nicht festgelegt werden", JOptionPane.ERROR_MESSAGE, this.errorImg));
 		}else {
-			this.emptyPassword = false;
-			this.newPasswordCorrect = true;
-		}
-		
-		
-		
-		//If the password match is identical and not empty password.
-		if(this.passwordMatch && !this.emptyPassword) {
-			
-			//iterate the char Array to check if we have empty spaces by the newPassword. 
-			for(int i=0; i< this.newPasswordCharArray.length; i++) {
-				
-				//Variable char store the current iterated index.
-				char currentIndex = this.newPasswordCharArray[i];
-				
-				//if currentIdex is empty(space)
-				if(currentIndex == ' ') {
-					
-					//We set the values
-					this.blankSpaceNewPassword = true;
-					this.newPasswordCorrect = false;
-				}
-				
-				
-				//We arrive to the End the newPasswordCharArray
-				if((i == newPasswordCharArray.length-1)) {
-					
-					//Evaluate if blankSpaceNewPassword = true 
-					if(this.blankSpaceNewPassword) {
-						
-						//New Thread to invoke and display an error message
-						SwingUtilities.invokeLater( () ->  JOptionPane.showMessageDialog(null, "Leerzeichen sind für die Passwort nicht zulässig\n\nVersuchen Sie es erneut mit einem gültigen Passwort "
-								   , "Kritischer Fehler Passwort Eingabe", JOptionPane.ERROR_MESSAGE, this.errorImg));	
-					
-						
-					//set value
-					this.newPasswordCorrect = false;
-					}
-					
-					
-				}
-			}
-		}
-		
-		
-		//If everything is OK with the password
-		if(this.newPasswordCorrect && this.passwordMatch && !this.emptyPassword) {
-			
-//			System.out.println("New Password: " + this.newPassword);
-//			System.out.println("Property to be stored: "+ this.passwordPropertyName );
-			
-			try {
-				
-				//encrypt the data
-				this.newPassword = this.dataEncryption.encryptData(this.newPassword);
-				this.passwordPropertyName = this.dataEncryption.encryptData(this.passwordPropertyName);
-				
-//				System.out.println("New Password: " + this.newPassword);
-//				System.out.println("Property to be stored: "+ this.passwordPropertyName );
-				
-				//Now we can call to write in the new value in the Properties inside the configuration File
-				//Initialize the PropertiesWriter instance
-				this.propertiesWriter = new PropertiesWriter();
-				
-				this.propertiesWriter.modifyAndWriteProperties(this.passwordPropertyName, this.newPassword);
 
-				
-				//Close Both GUIs
-				this.modifiyPasswordUserGUI.dispose();
-				
-				if(this.editUserGUI!=null) {
-					this.editUserGUI.dispose();
-				}
-				
-				
-				
-				
-			} catch (Exception e) {
-				
-				e.printStackTrace();
-			}
-			
+			//Everything is OK then we call to modifyNewPsswordInConfigurationFile.
+			modifyNewPasswordInConfigurationFile();
+
+
 		}
 		
 
@@ -495,6 +390,35 @@ public class LogicModelModifyPasswordUser {
   private void modifyNewPasswordInConfigurationFile() {
 	//if the password is correct we save the new value in the configuration file.
 
+	  try {
+			
+			//encrypt the data
+			this.newPassword = this.dataEncryption.encryptData(this.newPassword);
+			this.passwordPropertyName = this.dataEncryption.encryptData(this.passwordPropertyName);
+				
+			//Now we can call to write in the new value in the Properties inside the configuration File
+			//Initialize the PropertiesWriter instance
+			this.propertiesWriter = new PropertiesWriter();
+			
+			this.propertiesWriter.modifyAndWriteProperties(this.passwordPropertyName, this.newPassword);
+
+			
+			//Close Both GUIs
+			this.modifiyPasswordUserGUI.dispose();
+			
+			if(this.editUserGUI!=null) {
+				this.editUserGUI.dispose();
+			}
+			
+			
+			
+			
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}
+	  
+	  
   }
 	
 
