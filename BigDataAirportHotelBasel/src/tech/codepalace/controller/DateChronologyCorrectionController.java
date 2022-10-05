d09b29ac4b68b78074d9d952fb7318ecf85ada5b
Antonio Estevez Gonzalez <estevez75015@gmail.com>
2022-10-05 08:00:35 +0200

    DateChronologyCorrectionController implements..
    DateChronologyCorrectionController implements:
    
    - ActionListener;
    - KeyListener;
    - WindowListener;
    - FocusListener.

--- BigDataAirportHotelBasel/src/tech/codepalace/controller/DateChronologyCorrectionController.java
+++ BigDataAirportHotelBasel/src/tech/codepalace/controller/DateChronologyCorrectionController.java
@@ -1,9 +1,18 @@
 package tech.codepalace.controller;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
 import tech.codepalace.model.LogicModelDateChronologyCorrection;
 import tech.codepalace.view.frames.DateChronologyCorrection;
 
-public class DateChronologyCorrectionController {
+public class DateChronologyCorrectionController implements ActionListener, KeyListener, WindowListener, FocusListener {
 	
 	private DateChronologyCorrection dateChronologyCorrection;
 	LogicModelDateChronologyCorrection logicDateChronologyCorrection;
@@ -13,6 +22,96 @@ public class DateChronologyCorrectionController {
 		
 		this.dateChronologyCorrection = dateChronologyCorrection;
 		this.logicDateChronologyCorrection = logicModelDateChronologyCorrection;
+	}
+
+	
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+	}
+
+
+
+
+	@Override
+	public void keyTyped(KeyEvent e) {}
+
+
+
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		
+	}
+
+
+
+
+	@Override
+	public void keyReleased(KeyEvent e) {}
+
+
+
+
+	@Override
+	public void windowOpened(WindowEvent e) {}
+
+
+
+
+	@Override
+	public void windowClosing(WindowEvent e) {
+		
+	}
+
+
+
+
+	@Override
+	public void windowClosed(WindowEvent e) {}
+
+
+
+
+	@Override
+	public void windowIconified(WindowEvent e) {}
+
+
+
+
+	@Override
+	public void windowDeiconified(WindowEvent e) {}
+
+
+
+
+	@Override
+	public void windowActivated(WindowEvent e) {}
+
+
+
+
+	@Override
+	public void windowDeactivated(WindowEvent e) {}
+
+
+
+
+	@Override
+	public void focusGained(FocusEvent e) {
+		
+		
+	}
+
+
+
+
+	@Override
+	public void focusLost(FocusEvent e) {
+		
+		
 	}
 
 }
