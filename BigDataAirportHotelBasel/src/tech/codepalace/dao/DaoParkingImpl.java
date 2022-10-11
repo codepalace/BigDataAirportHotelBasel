9c4471901529162741667363bee4e6de87b48515
Antonio Estevez Gonzalez <captalohoy@gmail.com>
2022-10-11 21:37:10 +0200

    DisplayParking order by anreisedatum now(Arrival Date)

--- BigDataAirportHotelBasel/src/tech/codepalace/dao/DaoParkingImpl.java
+++ BigDataAirportHotelBasel/src/tech/codepalace/dao/DaoParkingImpl.java
@@ -607,7 +607,7 @@ public class DaoParkingImpl  implements DAOParking {
 		daoFactory.connect(); //agregado ultimo
 		
 		//Local variable to select all items in the Parking Table order by idparking ASC | DESC.
-		String sqlString = "SELECT * from PARKING ORDER BY idparking ASC";
+		String sqlString = "SELECT * from PARKING ORDER BY anreisedatum ASC";
 		
 		
 		try {
