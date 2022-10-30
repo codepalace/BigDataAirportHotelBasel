51c7147a0d9d6db539d57cf768d9c7d8e2d75de4
Antonio Estevez Gonzalez <captalohoy@gmail.com>
2022-10-30 14:34:42 +0100

    New Class Uebergabe HandOver shift informations transfer

---
+++ BigDataAirportHotelBasel/src/tech/codepalace/model/Uebergabe.java
@@ -0,0 +1,11 @@
+package tech.codepalace.model;
+
+/**
+ * 
+ * @author Antonio Estevez Gonzalez
+ * @description HandOver Class for the Shift information transfer
+ *
+ */
+public class Uebergabe {
+
+}
