558a06e877ca07f9e7a83fce50aac3a7d30ede64
Antonio Estevez Gonzalez <captalohoy@gmail.com>
2022-11-01 22:51:39 +0100

    New Class UebergabeTableModel new instructions by other Class.
    TopPanelUebergabe: New instructions to build the Top Panel HandOver
    Shift transfer informations TopPanel.
    
    UebergabeTableModel new Class.

---
+++ BigDataAirportHotelBasel/src/tech/codepalace/model/UebergabeTableModel.java
@@ -0,0 +1,62 @@
+package tech.codepalace.model;
+
+import javax.swing.table.DefaultTableModel;
+
+import tech.codepalace.utility.TableUebergabeUtilities;
+
+/**
+ * 
+ * @author Antonio Estevez Gonzalez
+ * @description Class to build the model 
+ *
+ */
+@SuppressWarnings("serial")
+public class UebergabeTableModel extends DefaultTableModel {
+
+String [] tableHeaders;
+	
+	//Data contained in the table
+	Object[][] data;
+	
+	/**
+	 * Determines the model with which the table is to be built
+	 * @param data
+	 * @param tableHeaders
+	 */
+	//Class Constructor receiving the data and titles
+	public UebergabeTableModel(Object[][] data, String[] tableHeaders){
+		super();
+		this.tableHeaders = tableHeaders;
+		this.data = data;
+		
+	
+		//We send the parameters to the setDataVector method where the model will be fed.
+		setDataVector(data, tableHeaders);
+	}
+	
+	
+	public UebergabeTableModel(){}
+	
+	/* We overwrite the isCellEditable method
+	  Method that receives the row and column where the user is at the moment.
+
+	 */
+	public boolean isCellEditable(int row, int column) {
+		
+		
+		/*
+	
+		The Constants have the value of each column where they are inside the table. Only by INFORMATION will be possible to edit
+		 */
+		if(column!=TableUebergabeUtilities.INFORMATION) {
+			return false;
+		}else {
+			return true;
+		}
+		
+	}
+
+	
+	
+	
+}
--- BigDataAirportHotelBasel/src/tech/codepalace/view/tables/UebergabeTable.java
+++ BigDataAirportHotelBasel/src/tech/codepalace/view/tables/UebergabeTable.java
@@ -1,5 +1,15 @@
 package tech.codepalace.view.tables;
 
+import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+
+import javax.swing.JTable;
+import javax.swing.border.BevelBorder;
+
+import tech.codepalace.model.Uebergabe;
+import tech.codepalace.model.UebergabeTableModel;
+
 /**
  * 
  * @author Antonio Estevez Gonzalez
@@ -7,5 +17,74 @@ package tech.codepalace.view.tables;
  *
  */
 public class UebergabeTable {
+	
+	private JTable uebergabeJTable;
+	
+	ArrayList<Uebergabe>listUebergabe; //HangOver list.
+	
+	UebergabeTableModel ubergabeTableModel;
+	
+	
+	
+	
+	public UebergabeTable() {
+		
+		init();
+		
+		buildTable();
+	}
+	
+	
+	private void init() {
+		
+		this.uebergabeJTable = new JTable() {
+			
+			
+			   
+
+			//Implement table cell tool tips.           
+	        public String getToolTipText(MouseEvent e) {
+	            String tip = null;
+	            java.awt.Point p = e.getPoint();
+	            int rowIndex = rowAtPoint(p);
+	            int colIndex = columnAtPoint(p);
+
+	            try {
+	                tip = getValueAt(rowIndex, colIndex).toString();
+	            } catch (RuntimeException e1) {
+	                //catch null pointer exception if mouse is over an empty line
+	            }
+
+	            return tip;
+	        }
+		};
+		
+		
+		uebergabeJTable.setBackground(Color.WHITE);
+		uebergabeJTable.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
+		
+		uebergabeJTable.setOpaque(false);
+		
+		
+	}
+	
+	
+	
+	
+	private void buildTable() {
+		
+		this.listUebergabe = new ArrayList<Uebergabe>();
+		
+		
+		/*
+		 Create the array of the titles, we will add the titles that are the columns
+		 */
+		ArrayList<String> tableHeadersList=new ArrayList<>();
+		
+		tableHeadersList.add("ID");
+		tableHeadersList.add("Datum");
+		tableHeadersList.add("Info");
+		tableHeadersList.add("Kürzel MA");
+	}
 
 }
